80471a11935885c1da556c989f3c0a4b99f0ccf1
khadija <Khadijabbirri20@gmail.com>
2022-06-14 21:54:57 +0100

    fix: fixed annotation

--- src/main/java/com/example/fpbm/repositories/CoursTdTpRepository.java
+++ src/main/java/com/example/fpbm/repositories/CoursTdTpRepository.java
@@ -2,6 +2,8 @@ package com.example.fpbm.repositories;
 
 import com.example.fpbm.entities.Courstdtp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CoursTdTpRepository extends JpaRepository<Courstdtp, Long> {
 }
--- src/main/java/com/example/fpbm/repositories/ExamenRepository.java
+++ src/main/java/com/example/fpbm/repositories/ExamenRepository.java
@@ -2,6 +2,8 @@ package com.example.fpbm.repositories;
 
 import com.example.fpbm.entities.Examen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
     public interface ExamenRepository extends JpaRepository<Examen, Long> {
 }
