a4048ae22bf072b6cedcaa5056ae55e2c2b849ec
Ray0Emma <ahmadifarah22@gmail.com>
2022-06-30 00:11:28 +0000

    bug: file pload

--- src/main/java/com/example/fpbm/controller/EtudiantController.java
+++ src/main/java/com/example/fpbm/controller/EtudiantController.java
@@ -2,15 +2,17 @@ package com.example.fpbm.controller;
 
 import com.example.fpbm.entities.Etudiant;
 import com.example.fpbm.services.EtudiantService;
+import com.example.fpbm.services.ExcelServices.EtudiantExcelImport;
 import com.example.fpbm.services.ExcelServices.IExcelDataService;
 import com.example.fpbm.services.ExcelServices.IFileUploaderService;
+import com.example.fpbm.services.FiliereService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+
 
 @RestController
 
@@ -22,33 +24,23 @@ public class EtudiantController {
     IFileUploaderService fileService;
 
     @Autowired
+    private FiliereService filiereService;
+
+    @Autowired
     IExcelDataService excelservice;
     @Autowired
     private EtudiantService etudiantService;
 
-    @PostMapping("/uploadFile")
-    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-
-        fileService.uploadFile(file);
-
-        redirectAttributes.addFlashAttribute("message",
-                "You have successfully uploaded '"+ file.getOriginalFilename()+"' !");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    @PostMapping(path="/uploadFile", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    @ResponseBody
+    public String importTransactionsFromExcelToDb(@RequestParam("file") List<MultipartFile> file) {
+        if(file.isEmpty()){
+            System.out.println("Empty File");
         }
+        EtudiantExcelImport a= new EtudiantExcelImport(etudiantService,filiereService);
+        a.importToDb(file);
         return "redirect:/";
-    }
 
-    @GetMapping("/saveData")
-    public String saveExcelData(Model model) {
-
-        List<Etudiant> excelDataAsList = excelservice.getExcelDataAsList();
-        int noOfRecords = excelservice.saveExcelData(excelDataAsList);
-        model.addAttribute("noOfRecords",noOfRecords);
-        return "success";
     }
 
     @GetMapping()
