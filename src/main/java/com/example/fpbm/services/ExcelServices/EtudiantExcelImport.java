ef2b03138d495a072c89462b918a82b39ad8c2c1
Ray0Emma <ahmadifarah22@gmail.com>
2022-07-05 20:42:35 +0000

    fix: added student attributes

--- src/main/java/com/example/fpbm/services/ExcelServices/EtudiantExcelImport.java
+++ src/main/java/com/example/fpbm/services/ExcelServices/EtudiantExcelImport.java
@@ -3,6 +3,7 @@ package com.example.fpbm.services.ExcelServices;
 import com.example.fpbm.entities.Etudiant;
 import com.example.fpbm.services.EtudiantService;
 import com.example.fpbm.services.FiliereService;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -10,6 +11,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +29,7 @@ public class EtudiantExcelImport {
     public void importToDb(List<MultipartFile> multipartfiles) {
         if (!multipartfiles.isEmpty()) {
             List<Etudiant> etudiants = new ArrayList<>();
+            DataFormatter formatter = new DataFormatter();
             multipartfiles.forEach(multipartfile -> {
                 try {
                     XSSFWorkbook workBook = new XSSFWorkbook(multipartfile.getInputStream());
@@ -38,38 +43,43 @@ public class EtudiantExcelImport {
                         if (rowIndex == 0) {
                             continue;
                         }
-                        String nom = row.getCell(0).getStringCellValue();
-                        String prenom = row.getCell(1).getStringCellValue();
-                        String cin = row.getCell(2).getStringCellValue();
-                        String cne = row.getCell(3).getStringCellValue();
-//                        long filiere= (long) row.getCell(4).getNumericCellValue();
+                        String email = row.getCell(0).getStringCellValue();
+                        String password = row.getCell(1).getStringCellValue();
+                        String address = row.getCell(2).getStringCellValue();
+                        String cin = row.getCell(3).getStringCellValue();
+                        String cne = row.getCell(4).getStringCellValue();
+//                        String birthdate = formatter.formatCellValue(row.getCell(5));
+                        String nom = row.getCell(6).getStringCellValue();
+                        String prenom = row.getCell(7).getStringCellValue();
+                        String tel = formatter.formatCellValue(row.getCell(8));
+                        String apogee = formatter.formatCellValue(row.getCell(9));
+                        long filiere= (long) row.getCell(10).getNumericCellValue();
 
                         Etudiant etudiant = new Etudiant();
+                        etudiant.setEmail(email);
+                        etudiant.setPassword(password);
+                        etudiant.setAddress(address);
+                        etudiant.setCin(cin);
+                        etudiant.setCne(cne);
+//                        etudiant.setDateDeNaissance(new SimpleDateFormat("yyyy-MM-dd").parse(birthdate));
                         etudiant.setNom(nom);
                         etudiant.setPrenom(prenom);
-                        etudiant.setCne(cne);
-                        etudiant.setCin(cin);
-//                        etudiant.setEtudiantCollectionFiliere(filiereService.getFiliere(filiere));
+                        etudiant.setTelephone(tel);
+                        etudiant.setNbApogee(apogee);
+                        etudiant.setFiliere(filiereService.getFiliere(filiere));
 
                         etudiants.add(etudiant);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+//                catch (ParseException e) {
+//                    throw new RuntimeException(e);
+//                }
             });
 
             if (!etudiants.isEmpty()) {
                 // save to database
-                etudiantService.saveAll(etudiants);
-            }else {
-                Etudiant etudiant = new Etudiant();
-                etudiant.setNom("farah");
-                etudiant.setPrenom("prenom");
-                etudiant.setCne("cne");
-                etudiant.setCin("cin");
-//                        etudiant.setEtudiantCollectionFiliere(filiereService.getFiliere(filiere));
-
-                etudiants.add(etudiant);
                 etudiantService.saveAll(etudiants);
             }
         }
