e6440b3408076118ad8de59333b7f8ed28c05df3
koliche <koliche.mohamed0@gmail.com>
2022-08-24 18:47:26 +0100

    add generate Pvs Condition

--- src/main/java/com/example/fpbm/repositories/pvRepository/PvRepository.java
+++ src/main/java/com/example/fpbm/repositories/pvRepository/PvRepository.java
@@ -4,6 +4,11 @@ import com.example.fpbm.modeles.Pv;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PvRepository extends JpaRepository<Pv, Long> {
+
+    List<Pv> getAllByFilierAndSemesterAndModuleAndLocalDateTime(String filiere, String semestre, String module, String time);
+
 }
