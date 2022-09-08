a325e8393beabf225cc5466d199cb9e98333d834
Ray0Emma <ahmadifarah22@gmail.com>
2022-09-08 17:07:16 +0100

    fix: added CrossOrigin

--- src/main/java/com/example/fpbm/controller/pvControler/OrderedPvsController.java
+++ src/main/java/com/example/fpbm/controller/pvControler/OrderedPvsController.java
@@ -6,13 +6,11 @@ import com.example.fpbm.modeles.Pv;
 import com.example.fpbm.services.entitiesServices.EtudiantService;
 import com.example.fpbm.services.PvServices.OrderedPvsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/orderPv")
 public class OrderedPvsController {
